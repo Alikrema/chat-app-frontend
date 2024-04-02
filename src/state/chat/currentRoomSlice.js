32ab7321fb3e14d635d29c821fd1ab610b2643d5
alikrema <alimk1741@gmail.com>
2024-04-02 15:34:06 +0200

    Add state management for current room

---
+++ src/state/chat/currentRoomSlice.js
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getRoom, getRoomMessages } from "../../api/chat/chatServices";
+
+export const getRoomData = createAsyncThunk(
+  "chat/getRoomData",
+  async (roomId, thunkAPI) => {
+    try {
+      const room = await getRoom(roomId);
+      const messages = await getRoomMessages(roomId);
+      return { room, messages };
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const currentRoomSlice = createSlice({
+  name: "currentRoom",
+  initialState: {
+    room: {},
+    messages: [],
+    status: "idle",
+    error: null,
+    loading: true,
+  },
+  reducers: {
+    setRoom: (state, action) => {
+      state.room = action.payload;
+      state.loading = false;
+    },
+    setMessages: (state, action) => {
+      state.messages = action.payload;
+      state.loading = false;
+    },
+    clearRoom: (state) => {
+      state.room = {};
+      state.messages = [];
+      state.loading = false;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRoomData.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(getRoomData.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.room = action.payload.room;
+        state.messages = action.payload.messages;
+        state.error = null;
+      })
+      .addCase(getRoomData.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setRoom, setMessages, clearRoom, setLoading } =
+  currentRoomSlice.actions;
+export default currentRoomSlice.reducer;
--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice.js";
 import chatRoomsReducer from "./chat/chatRoomsSlice.js";
+import currentRoomReducer from "./chat/currentRoomSlice.js";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     chatRooms: chatRoomsReducer,
+    currentRoom: currentRoomReducer,
   },
 });
